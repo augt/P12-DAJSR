8f96a05e76d0620e1181fbd488e13669f18c7054
Augustin Tacquet <augu.tacquet@gmail.com>
2023-09-15 11:03:13 +0200

    display user's name

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import SideBar from "../../components/SideBar";
 import DashboardCSS from "./Dashboard.module.css";
+import { userPersonalData } from "../../utils/mockedData";
 
 function Dashboard() {
   return (
     <main>
-      <SideBar /> <div>Dashboard</div>
+      <SideBar />
+      <div className={DashboardCSS.margins}>
+        <div className={DashboardCSS.welcome_message}>
+          Bonjour{" "}
+          <span className={DashboardCSS.user_firstname}>
+            {userPersonalData.userInfos.firstName}
+          </span>
+        </div>
+        <div className={DashboardCSS.cheering_message}>
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </div>
+        <div></div>
+      </div>
     </main>
   );
 }
---
+++ src/utils/mockedData.js
@@ -0,0 +1,14 @@
+export const userPersonalData = {
+  userInfos: {
+    firstName: "Thomas",
+    lastName: "Dovineau",
+    age: 31,
+  },
+  todayScore: 0.12,
+  keyData: {
+    calorieCount: 1930,
+    proteinCount: 155,
+    carbohydrateCount: 290,
+    lipidCount: 50,
+  },
+};
